70fe0ebd4fceb2f2ed454ba6275b4a61b768b6f8
Ariful Islam <ariful6@yahoo.com>
2024-06-27 07:18:29 +0300

    showing null when params is missing

--- web-soft-dev/app.js
+++ web-soft-dev/app.js
@@ -2,24 +2,7 @@ const handleRequest = (request) => {
   const url = new URL(request.url);
   const params = url.searchParams;
 
-  const operation = params.get("operation");
-  const number1 = Number(params.get("number1"));
-  const number2 = Number(params.get("number2"));
-
-  const product = number1 * number2;
-  const sum = number1 + number2;
-
-  if (operation && number1 && number2) {
-    if (operation === "sum") {
-      return new Response(`${sum}`);
-    } else if (operation === "product") {
-      return new Response(`${product}`);
-    } else {
-      return new Response("Invalid parameters..");
-    }
-  } else {
-    return new Response("Invalid parameters..");
-  }
+  return new Response(`${params.get("page")} ${params.get("count")}`);
 };
 
 Deno.serve(handleRequest);
