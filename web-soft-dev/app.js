c9f104af7fb79b05b604e1e2f5f8d44145f4bf2e
Ariful Islam <ariful6@yahoo.com>
2024-06-27 06:44:06 +0300

    ex-2

--- web-soft-dev/app.js
+++ web-soft-dev/app.js
@@ -1,6 +1,12 @@
 const handleRequest = (request) => {
   const url = new URL(request.url);
-  return new Response(`${request.method} request made to path ${url.pathname}`);
+
+  let message = "There is nothing to see here...";
+  if (request.method === "PEEK" && url.pathname === "/secret") {
+    message = "Peeking at secret data...";
+  }
+
+  return new Response(message);
 };
 
 Deno.serve(handleRequest);
