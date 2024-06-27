22e092bcc9f3b5b74940f160173015e7294e3726
Ariful Islam <ariful6@yahoo.com>
2024-06-27 07:47:59 +0300

    search params and params.has method

--- web-soft-dev/app.js
+++ web-soft-dev/app.js
@@ -2,7 +2,17 @@ const handleRequest = (request) => {
   const url = new URL(request.url);
   const params = url.searchParams;
 
-  return new Response(`${params.get("page")} ${params.get("count")}`);
+  let title = "princess";
+  let name = "Tove";
+
+  if (params.has("title")) {
+    title = params.get("title");
+  }
+  if (params.has("name")) {
+    name = params.get("name");
+  }
+
+  return new Response(`Once upon a time, there was a ${title} called ${name}.`);
 };
 
 Deno.serve(handleRequest);
