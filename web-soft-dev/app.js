fe7c0803641def393d1a0b1fc812dca985ba2647
Ariful Islam <ariful6@yahoo.com>
2024-06-27 07:06:29 +0300

    calculator from url params

--- web-soft-dev/app.js
+++ web-soft-dev/app.js
@@ -2,10 +2,24 @@ const handleRequest = (request) => {
   const url = new URL(request.url);
   const params = url.searchParams;
 
-  const one = Number(params.get("one"));
-  const two = Number(params.get("two"));
+  const operation = params.get("operation");
+  const number1 = Number(params.get("number1"));
+  const number2 = Number(params.get("number2"));
 
-  return new Response(`Sum: ${one + two}`);
+  const product = number1 * number2;
+  const sum = number1 + number2;
+
+  if (operation && number1 && number2) {
+    if (operation === "sum") {
+      return new Response(`${sum}`);
+    } else if (operation === "product") {
+      return new Response(`${product}`);
+    } else {
+      return new Response("Invalid parameters..");
+    }
+  } else {
+    return new Response("Invalid parameters..");
+  }
 };
 
 Deno.serve(handleRequest);
