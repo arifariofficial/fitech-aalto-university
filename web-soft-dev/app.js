3256a96d1725f513740863d78b452c93994f4429
Ariful Islam <ariful6@yahoo.com>
2024-06-27 06:48:24 +0300

    Extracting request parameters

--- web-soft-dev/app.js
+++ web-soft-dev/app.js
@@ -1,12 +1,5 @@
 const handleRequest = (request) => {
-  const url = new URL(request.url);
-
-  let message = "There is nothing to see here...";
-  if (request.method === "PEEK" && url.pathname === "/secret") {
-    message = "Peeking at secret data...";
-  }
-
-  return new Response(message);
+  return new Response(request.url);
 };
 
 Deno.serve(handleRequest);
