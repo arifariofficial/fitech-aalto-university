3f0288f9abd0c60bc7db70a3a7098eae8bb75e2a
Ariful Islam <ariful6@yahoo.com>
2024-06-27 06:52:13 +0300

    params with numbers

--- web-soft-dev/app.js
+++ web-soft-dev/app.js
@@ -1,7 +1,11 @@
 const handleRequest = (request) => {
   const url = new URL(request.url);
   const params = url.searchParams;
-  return new Response(`Name: ${params.get("name")}`);
+
+  const one = Number(params.get("one"));
+  const two = Number(params.get("two"));
+
+  return new Response(`Sum: ${one + two}`);
 };
 
 Deno.serve(handleRequest);
