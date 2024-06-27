09ea99091272ad48609655cb911ef5c9c01e4f61
Ariful Islam <ariful6@yahoo.com>
2024-06-27 04:21:04 +0300

    Path-specific responses

--- web-soft-dev/app.js
+++ web-soft-dev/app.js
@@ -1,1 +1,14 @@
-Deno.serve(() => new Response("Hello world!"));
+const handleRequest = (request) => {
+  const url = new URL(request.url);
+
+  let message = "hello";
+  if (url.pathname === "/hello") {
+    message = "world";
+  } else if (url.pathname.includes("secret")) {
+    message = "ingredient";
+  }
+
+  return new Response(message);
+};
+
+Deno.serve(handleRequest);
